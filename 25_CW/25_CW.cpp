8561fcff5d3451b1b9a955e8830e908f34a934b4
paulbardar <Q.C.tulip@gmail.com>
2026-04-29 00:31:06 +0300

    vector

--- 25_CW/25_CW.cpp
+++ 25_CW/25_CW.cpp
@@ -2,10 +2,14 @@
 
 #include "MyVector.h"
 #include <string>
+#include <vector>
+#include <algorithm>
+#include <cstdlib>
 
 
 int main()
 {
+    /*
     MyVector<int> a(3, 0); //size = 3, arr = [0, 0 , 0]
     MyVector <string> b; //size = 0, arr = []
 
@@ -22,6 +26,38 @@ int main()
 
     a.print();
     b.print();
+    */
+
+    vector<int> v1;
+    for (int i = 0; i < 10; i++) {
+        v1.push_back(rand() % 10 - 10);
+    }
+
+    cout << "Size: " << v1.size() << endl;
+    
+
+    // Способи роботи з елементами вектору
+    vector<int>::iterator it; // Альтернатива покажчику
+    for (it = v1.begin(); it != v1.end(); it++) {
+        cout << *it << " ";
+    } cout << endl;
+
+    for (int i = 0; i < v1.size(); i++) {
+        cout << v1[i] << " ";
+    } cout << endl;
+    
+    
+    for (int a : v1) { //[4, 7, 2, 5, 7]  // get
+        cout << a << " ";
+    } cout << endl;
+
+    for (int& a : v1) {  //[4, 7, 2, 5, 7]  // get + set
+        cout << a << " ";
+    }cout << endl;
+
+    for (auto& a : v1) {
+        cout << a << " ";
+    } cout << endl;
 
     return 0;
 }
